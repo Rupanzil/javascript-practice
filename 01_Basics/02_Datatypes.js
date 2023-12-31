3502acbb28ede64173708dcd56199a2cd7a9ffe9
Rupanzil <153161192+Rupanzil@users.noreply.github.com>
2023-12-31 12:46:14 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,11 @@
+//Some additional datatypes other than number, boolean and string
+// null is a standalone value representing empty value
+
+let anEmptyValue = null;    //This has the datatype of object
+let anEmptyString = "";     //This has a string datatype
+let a;    // This is undefined datatype
+
+console.log(typeof(anEmptyString));
+console.log(typeof(anEmptyValue));
+console.log(typeof(a));
+
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,18 @@
+"use strict";  // To use the current/latest JS syntax (ES6)
+
+
 console.log("My name is Rupanzil! ");
+const accountId = "31122023";
+let accountName = "Rupanzil";
+let accountEmail = "rupanzilprince@gmail.com";
+
+console.table([accountId, accountEmail, accountName, "this is just a string!"]);    //The contents of the console.table should be inside []
+
+//console.table() command prints an array in a tabular format
+
+let arrayOfFruits = ["Apple", "Guava", "Mango", "Lichi", "Strawberry"];
+
+console.table(arrayOfFruits);
+
+//alert can't be used inside node.js
+// alert("Hi!!");
